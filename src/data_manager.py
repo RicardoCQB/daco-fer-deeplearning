e58ce3fcca8b728c92c4a081227fcf5355001074
Ricardo Brioso <ricardo_cqb@hotmail.com>
2020-12-01 18:23:46 +0000

    it reads the csv

---
+++ src/data_manager.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import csv
+
+''' This section reads the dataset from the .csv file in the fer2013 folder '''
+data_path_list = ["C:/Users/Ricardo/source/repos/daco-fer-deeplearning/data/fer2013/fer2013.csv", "/Users/esmeraldacruz/Documents/GitHub/daco-fer-deeplearning/data/fer2013/fer2013.csv","C:\\Users\\dtrdu\\Desktop\\Duarte\\Faculdade e Cadeiras\\DACO\\Project\\daco-fer-deeplearning\\data\\fer2013\\fer2013.csv" ]
+data=[]
+data = pd.read_csv(data_path_list[0])
+
+''' The .csv file consists of the pixels of the 48x48 pixels image, and it also
+has a label that determines each emotion and the purpose of the image (train or test) '''
+
+#Atribution of each label to an emotion
+emotions_dic = {0: 'Angry', 1: 'Disgust', 2: 'Fear', 3: 'Happy', 4: 'Sad', 5: 'Surprise', 6: 'Neutral'}
