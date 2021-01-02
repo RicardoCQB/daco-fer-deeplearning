7a59cf9b60b8785631cfc879ece583be3db16ff0
RicardoCQB <ricardo_cqb@hotmail.com>
2021-01-02 19:58:56 +0000

    mamamiaPizzaria

--- src/main.py
+++ src/main.py
@@ -1,25 +1,26 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import csv
 from PIL import Image
 from sklearn.model_selection import train_test_split
-import torch.nn
+
 from models import resnet
 from utils.utils import *
-import torchvision
 
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import torch.backends.cudnn as cudnn
-from torch.utils.data import Dataset, DataLoader
+from sklearn.metrics import confusion_matrix
+from keras.models import Sequential, load_model
+from keras.layers import Conv2D, MaxPooling2D, Dense, Activation, Dropout, Flatten, BatchNormalization
+from keras.optimizers import Adam
+from keras.initializers import RandomNormal
+from keras.callbacks import ModelCheckpoint, TensorBoard
+from keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
+
 
 ''' This section reads the dataset from the .csv file in the fer2013 folder '''
 data_path_list = ["C:/Users/Ricardo/source/repos/daco-fer-deeplearning/data/fer2013/fer2013.csv", "/Users/esmeraldacruz/Documents/GitHub/daco-fer-deeplearning/data/fer2013/fer2013.csv","C:\\Users\\dtrdu\\Desktop\\Duarte\\Faculdade e Cadeiras\\DACO\\Project\\daco-fer-deeplearning\\data\\fer2013\\fer2013.csv", "C:/Users/Ricardo/source/daco-fer-deeplearning/data/fer2013/fer2013.csv"]
 data=[]
-data = pd.read_csv(data_path_list[3], nrows = 300)
+data = pd.read_csv(data_path_list[3], nrows = 1000)
 
 
 ''' The .csv file consists of the pixels of the 48x48 pixels image, and it also
@@ -55,77 +56,101 @@ labels_count = np.unique(labels_flat).shape[0]
 labels = dense_to_one_hot(labels_flat, labels_count)
 labels = labels.astype(np.uint8)
 
-
 '''Image reshaping and dataset splitting'''
 # Reshaping and preparing image format for the model training.
 images = images.reshape(images.shape[0], 48, 48, 1)
 images = images.astype('float32')
-train_loader = torch.utils.data.DataLoader(images, batch_size=16, shuffle=True, num_workers=0) # num_workers???
 
-
-# Splitting images and labels into training, validation and testing sets
 X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.1, shuffle = False)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.1, shuffle = False)
 
 #Note: if we are doing k cross validation then the val split is unnecessary and we need to substitute it.
 
 ''' This part of the code is for building the CNN model we are using  for the train'''
-net = resnet.ResNet18()
+# Constructing CNN structure
+model = Sequential()
 
-criterion = nn.CrossEntropyLoss()
-optim = 'Adam'
-lr=1e-3  # Default learning rate
-weight_decay=1e-4
-epoch = 150
-logspace = 0
+# 1st convolution layer
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=(48, 48, 1),
+                 bias_initializer=RandomNormal(stddev=1), kernel_initializer=RandomNormal(stddev=1)))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=(48, 48, 1),
+                 bias_initializer=RandomNormal(stddev=1), kernel_initializer=RandomNormal(stddev=1)))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+model.add(Dropout(0.25))
 
-if optim == 'SGD':
-    optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9, weight_decay=weight_decay)
-elif optim == 'Adam':
-    optimizer = optim.Adam(net.parameters(), lr=lr, weight_decay=weight_decay)
-logspace_lr = torch.logspace(np.log10(lr), np.log10(lr) - logspace, epoch)
+# 3rd convolution layer
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', bias_initializer=RandomNormal(stddev=1),
+                 kernel_initializer=RandomNormal(stddev=1)))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', bias_initializer=RandomNormal(stddev=1),
+                 kernel_initializer=RandomNormal(stddev=1)))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+model.add(Dropout(0.25))
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-best_acc_val = 0  # best test accuracy
-best_acc_test = 0  # best test accuracy
-start_epoch = 0  # start from epoch 0
+# 5th convolution layer
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same', bias_initializer=RandomNormal(stddev=1),
+                 kernel_initializer=RandomNormal(stddev=1)))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same', bias_initializer=RandomNormal(stddev=1),
+                 kernel_initializer=RandomNormal(stddev=1)))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+model.add(Dropout(0.25))
+
+# 7th convolution layer
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same', bias_initializer=RandomNormal(stddev=1),
+                 kernel_initializer=RandomNormal(stddev=1)))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same', bias_initializer=RandomNormal(stddev=1),
+                 kernel_initializer=RandomNormal(stddev=1)))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Flatten())
+# Fully connected layers
+model.add(Dense(2048, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(labels_count, activation='softmax'))
+
+# Compiling model
+model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
+
+model.summary()
+
+# Specifying parameters for Data Augmentation
+datagen = ImageDataGenerator(
+    featurewise_center=False,  # set input mean to 0 over the dataset
+    samplewise_center=False,  # set each sample mean to 0
+    featurewise_std_normalization=False,  # divide inputs by std of the dataset
+    samplewise_std_normalization=False,  # divide each input by its std
+    zca_whitening=False,  # apply ZCA whitening
+    rotation_range=40,  # randomly rotate images in the range (degrees, 0 to 180)
+    width_shift_range=0.2,  # randomly shift images horizontally (fraction of total width)
+    height_shift_range=0.2,  # randomly shift images vertically (fraction of total height)
+    horizontal_flip=True,  # randomly flip images
+    vertical_flip=False,
+    zoom_range = 0.05)  # zoom images in range [1 - zoom_range, 1+ zoom_range]
+
+datagen.fit(X_train)
 
 
-# Training
-def train(epoch):
-    print('\nEpoch: %d' % epoch)
-    net.train()
+# Saving model each time it achieves lower loss on the validation set
+filepath = 'Model.1.hdf5'
+checkpointer = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
+tensorboard = TensorBoard(log_dir='./logs')
 
-    train_loss = 0
-    correct = 0
-    total = 0
-    loss_res = 0
 
-    adjust_learning_rate(optimizer, epoch, lr)
+history = model.fit_generator(datagen.flow(X_train, y_train,
+                    batch_size=16),
+                    epochs=10,
+                    validation_data=(X_val, y_val),
+                    steps_per_epoch=X_train.shape[0]/32,
+                    callbacks=[checkpointer,tensorboard]),
 
-    for batch_idx, (inputs, targets) in enumerate(train_loader):
-        inputs, targets = inputs.to(device), targets.to(device)
-        optimizer.zero_grad()
-        outputs = net(inputs)
-        prob = F.softmax(outputs, dim=1)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        # clip_grad_norm_(parameters=net.parameters(), max_norm=0.1)
 
-        optimizer.step()
+pd.DataFrame(history).to_csv("history.csv")
 
-        train_loss += loss.item()
-        loss_res = train_loss / (batch_idx + 1)
 
-        max_num, predicted = prob.max(1)
-
-        total += targets.size(0)
-        correct += predicted.eq(targets).sum().item()
-        progress_bar(batch_idx, len(train_loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                     % (train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
-
-        # print('Loss: %.3f | Acc: %.3f%% (%d/%d)' % (train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
-    return loss_res
 #TODO: Do stratified k cross fold validation
 #TODO: Build and compile model
 #TODO: Try and test to see accuracy with resnet18
